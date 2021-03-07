c3b2264972776f2aa1173f60f1702a10b401d648
jojoford <75343703+jojoford@users.noreply.github.com>
2021-03-07 16:12:45 -0600

    Create server.js

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const apiRoutes = require('./routes/noteRoutes');
+const htmlRoutes = require('./routes/index');
+
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static('public'));
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
